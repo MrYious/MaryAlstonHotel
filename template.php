64afa561ee5af84e9d8aaad812d99fd9e1f0ab0b
MrYious <rosariomark37@gmail.com>
2022-10-07 16:21:10 +0800

    feat: update template file

---
+++ template.php
@@ -0,0 +1,20 @@
+<?php
+	// session_start();
+	// if(!isset($_SESSION['username']) && !isset($_SESSION['password'])){
+	// 	header('location:alston_main.html');
+	// }
+?>
+<html>
+    <head>
+        <title> Title </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="js/jquery-3.6.1.min.js"></script>
+        <link rel="icon" href="images/favicon.png" type = "image/png">
+    </head>
+    <body>
+        <section id="" class="flex ">
+            Section
+        </section>
+    </body>
+</html>
