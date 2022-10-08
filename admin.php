e8d380173305c97f8511f9148b02d75b24be861f
MrYious <rosariomark37@gmail.com>
2022-10-09 00:07:09 +0800

    feat: admin login page

--- admin.php
+++ admin.php
@@ -1,8 +1,5 @@
 <?php
-	// session_start();
-	// if(!isset($_SESSION['username']) && !isset($_SESSION['password'])){
-	// 	header('location:alston_main.html');
-	// }
+	session_start();
 ?>
 <html>
     <head>
@@ -30,22 +27,50 @@
 
     <body>
         <main class="flex justify-center items-center w-full min-h-screen bg-[url('/images/gallery.jpg')] bg-cover">
-            <form action="" method="POST" class="flex flex-col items-center gap-5 p-6 border-2 border-slate-100 text-lg bg-slate-100 drop-shadow-2xl shadow-2xl shadow-black rounded-lg">
-                <div class="text-3xl font-semibold text-center ">Administrator </div>
-                <div class="flex justify-center items-center flex-col gap-3 w-full">
-                    <!-- ERROR MESSAGE -->
-                    <!-- <div class="text-xs bg-red-400 py-1 px-3 rounded-full text-center">Incorrect username or password</div> -->
-                    <div class="flex justify-center items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 " viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
-                        </svg>
-                        <input type="text" name="username" id="username" placeholder="Username" class="border-[1px] border-black p-2 text-sm focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none rounded" maxlength="15" size="25" required autocomplete="username">
+            <form action="api/validateLogin.php" method="POST" class="flex flex-col items-center gap-3 p-6 border-2 border-slate-100 text-lg bg-slate-100 drop-shadow-2xl shadow-2xl shadow-black rounded-lg">
+                <div class="text-2xl lg:text-3xl font-semibold text-center ">Administrator </div>
+                <!-- ERROR MESSAGE -->
+                <?php
+                    if (isset($_SESSION["errorMsg"])) {
+                        echo '<div class="text-sm bg-red-400 py-1 px-3 rounded-full text-center">' . $_SESSION["errorMsg"] . '</div>';
+                    }
+                ?>
+                <div class="flex justify-center items-center flex-col w-64 lg:w-72 text-sm lg:text-base">
+                    <div class="form-floating mb-3 w-full">
+                        <input type="text" name="username" class="form-control
+                            block
+                            w-full
+                            px-3
+                            py-1.5
+                            font-normal
+                            text-gray-700
+                            bg-white bg-clip-padding
+                            border border-solid border-gray-300
+                            rounded
+                            transition
+                            ease-in-out
+                            m-0
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="floatingInput" placeholder="Username" required maxlength="15" minlength="3"
+                        >
+                        <label for="floatingInput" class="text-gray-700">Username</label>
                     </div>
-                    <div class="flex justify-center items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
-                        </svg>
-                        <input type="password" name="password" id="password" placeholder="Password" class="border-[1px] border-black p-2 text-sm focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none rounded" maxlength="15" size="25" required>
+                    <div class="form-floating mb-3 w-full">
+                        <input type="password" name="password" class="form-control
+                            block
+                            w-full
+                            px-3
+                            py-1.5
+                            font-normal
+                            text-gray-700
+                            bg-white bg-clip-padding
+                            border border-solid border-gray-300
+                            rounded
+                            transition
+                            ease-in-out
+                            m-0
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="floatingPassword" placeholder="Password" required maxlength="15" minlength="5"
+                        >
+                        <label for="floatingPassword" class="text-gray-700">Password</label>
                     </div>
                 </div>
                 <input type="submit" value="LOGIN" class="p-2 w-full cursor-pointer bg-green-700 shadow-md shadow-black text-white font-semibold tracking-widest  hover:bg-green-900">
