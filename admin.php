da49c3aedc342ed803cda23706851197faac0eb8
MrYious <rosariomark37@gmail.com>
2022-10-08 20:26:24 +0800

    feat: add tailwind elements

--- admin.php
+++ admin.php
@@ -12,6 +12,20 @@
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="js/jquery-3.6.1.min.js"></script>
         <link rel="icon" href="images/favicon.png" type = "image/png">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+        <script>
+            tailwind.config = {
+                theme: {
+                    extend: {
+                        fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                        },
+                    }
+                }
+            }
+        </script>
     </head>
 
     <body>
