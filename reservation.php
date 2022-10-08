29afe33cd05c04b3cb552e8f70d3b747690359d2
MrYious <rosariomark37@gmail.com>
2022-10-08 18:26:34 +0800

    feat: initial layout of reservation form

---
+++ reservation.php
@@ -0,0 +1,21 @@
+<?php
+	// session_start();
+	// if(!isset($_SESSION['username']) && !isset($_SESSION['password'])){
+	// 	header('location:alston_main.html');
+	// }
+?>
+<html>
+    <head>
+        <title> Mary Alston Hotel </title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="js/jquery-3.6.1.min.js"></script>
+        <link rel="icon" href="images/favicon.png" type = "image/png">
+    </head>
+    <body>
+        <section id="" class="flex ">
+            Section
+        </section>
+    </body>
+</html>
