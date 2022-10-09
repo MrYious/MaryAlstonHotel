bb0f57fa0602623fda8cab740cd0e90f577f1ef2
MrYious <rosariomark37@gmail.com>
2022-10-09 14:59:50 +0800

    feat: initial dashboard

---
+++ admin/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+	include dirname(__DIR__).'/api/authSession.php';
+?>
+<html>
+    <head>
+        <title> Dashboard </title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="js/jquery-3.6.1.min.js"></script>
+        <link rel="icon" href="images/favicon.png" type = "image/png">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+        <script>
+            tailwind.config = {
+                theme: {
+                    extend: {
+                        fontFamily: {
+                            sans: ['Inter', 'sans-serif'],
+                        },
+                    }
+                }
+            }
+        </script>
+    </head>
+    <body>
+        <section id="" class="flex justify-center items-center min-h-screen">
+            <a href="/api/logout.php" class="p-3 bg-red-500">Log out</a>
+        </section>
+    </body>
+</html>
