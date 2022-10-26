7c3694b7a60f02c155d7050412f249e889d07e99
MrYious <rosariomark37@gmail.com>
2022-10-26 19:38:32 +0800

    feat: added template files for other pages

---
+++ admin/reports/monthly.php
@@ -0,0 +1,126 @@
+<?php
+	session_start();
+    $dir = dirname(__DIR__);
+	include trim($dir,'\admin').'/api/authSession.php';
+?>
+<html>
+    <head>
+        <title> Title </title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="js/jquery-3.6.1.min.js"></script>
+        <link rel="icon" href="images/favicon.png" type = "image/png">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"
+        />
+        <script>
+            tailwind.config = {
+                theme: {
+                    extend: {
+                        fontFamily: {
+                            sans: ['Inter', 'sans-serif'],
+                        },
+                    }
+                }
+            }
+        </script>
+    </head>
+    <body>
+        <!-- Mobile Nav Button -->
+        <span class="fixed text-white text-4xl top-5 left-4 cursor-pointer" onclick="openSidebar()" >
+            <div class="p-1 bg-gray-900 rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                </svg>
+            </div>
+        </span>
+
+        <div class="flex">
+            <!-- NAV -->
+            <div class="bg-gray-200 lg:w-[300px] shrink-0">
+                <div class="sidebar fixed top-0 bottom-0 left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-800">
+                    <div class="text-gray-100 text-xl">
+                        <div class="p-2.5 mt-1 flex justify-between items-center">
+                            <h1 class="font-bold text-gray-200 text-[20px] ml-3">MARY ALSTON HOTEL</h1>
+                            <i class="bi bi-x-lg cursor-pointer lg:hidden" onclick="openSidebar()"></i>
+                        </div>
+                        <div class="my-2 bg-gray-600 h-[1px]"></div>
+                    </div>
+                    <a href="/admin/dashboard.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-columns"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Dashboard</span>
+                    </a>
+                    <a href="/admin/calendar.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-calendar-week"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Official Calendar</span>
+                    </a>
+                    <a href="/admin/reschedule.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-arrow-repeat"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Reschedule</span>
+                    </a>
+                    <a href="/admin/reservations.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-calendar-plus"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Pending Reservations</span>
+                    </a>
+                    <a href="/admin/history.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-clock-history"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">History</span>
+                    </a>
+                    <div class="my-4 bg-gray-600 h-[1px]"></div>
+                    <div onclick="dropdown()" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-bar-chart"></i>
+                        <div class="flex justify-between w-full items-center">
+                            <span class="text-[15px] ml-4 text-gray-200 font-bold">Reports</span>
+                            <span class="text-sm rotate-180" id="arrow">
+                                <i class="bi bi-chevron-down"></i>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu">
+                        <a href="/admin/reports/monthly.php"  class="block cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                            Monthly
+                        </a>
+                        <a href="/admin/reports/weekly.php"  class="block cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                            Weekly
+                        </a>
+                    </div>
+                    <a href="/admin/settings.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-gear"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Settings</span>
+                    </a>
+                    <a href="/api/logout.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-box-arrow-in-right"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+                    </a>
+                </div>
+            </div>
+            <!-- CONTENT -->
+            <div class="w-full">
+                <section id="" class="flex justify-center items-center min-h-screen bg-gray-200">
+                    Section 1
+                </section>
+                <section id="" class="flex justify-center items-center min-h-screen bg-gray-200">
+                    Section 2
+                </section>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+        <script type="text/javascript">
+            function dropdown() {
+                document.querySelector("#submenu").classList.toggle("hidden");
+                document.querySelector("#arrow").classList.toggle("rotate-0");
+            }
+            dropdown();
+
+            function openSidebar() {
+                document.querySelector(".sidebar").classList.toggle("hidden");
+            }
+        </script>
+    </body>
+</html>
