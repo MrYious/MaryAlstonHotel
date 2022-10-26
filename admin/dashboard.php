b5b209df65de5b8e9818cef3c67b328c2199d911
MrYious <rosariomark37@gmail.com>
2022-10-26 10:35:23 +0800

    feat: updated sidebar

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -13,6 +13,10 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"
+        />
         <script>
             tailwind.config = {
                 theme: {
@@ -26,9 +30,97 @@
         </script>
     </head>
     <body>
-        <section id="" class="flex justify-center items-center min-h-screen">
+        <!-- <section id="" class="flex justify-center items-center min-h-screen bg-slate-200">
             <a href="/api/logout.php" class="p-3 bg-red-500">Log out</a>
-        </section>
+
+        </section> -->
+
+        <!-- Mobile Button -->
+        <span class="absolute text-white text-4xl top-5 left-4 cursor-pointer" onclick="openSidebar()" >
+            <div class="p-1 bg-gray-900 rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                </svg>
+            </div>
+        </span>
+
+        <div class="flex">
+            <div class="bg-green-600 lg:w-[300px] shrink-0">
+                <div class="sidebar fixed top-0 bottom-0 left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-800">
+                    <div class="text-gray-100 text-xl">
+                        <div class="p-2.5 mt-1 flex justify-between items-center">
+                            <h1 class="font-bold text-gray-200 text-[20px] ml-3">MARY ALSTON HOTEL</h1>
+                            <i class="bi bi-x-lg cursor-pointer lg:hidden" onclick="openSidebar()"></i>
+                        </div>
+                        <div class="my-2 bg-gray-600 h-[1px]"></div>
+                    </div>
+                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-columns"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Dashboard</span>
+                    </div>
+                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-calendar-week"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Official Calendar</span>
+                    </div>
+                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-arrow-repeat"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Reschedule</span>
+                    </div>
+                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-calendar-plus"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Pending Reservations</span>
+                    </div>
+                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-clock-history"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">History</span>
+                    </div>
+                    <div class="my-4 bg-gray-600 h-[1px]"></div>
+                    <div onclick="dropdown()" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-bar-chart"></i>
+                        <div class="flex justify-between w-full items-center">
+                            <span class="text-[15px] ml-4 text-gray-200 font-bold">Reports</span>
+                            <span class="text-sm rotate-180" id="arrow">
+                                <i class="bi bi-chevron-down"></i>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu">
+                        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                            Monthly
+                        </h1>
+                        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                            Weekly
+                        </h1>
+                    </div>
+                    <a href="/api/logout.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-box-arrow-in-right"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+                    </a>
+                </div>
+            </div>
+            <div class="w-full">
+                <section id="" class="flex justify-center items-center min-h-screen bg-gray-200">
+                    Section 1
+                </section>
+                <section id="" class="flex justify-center items-center min-h-screen bg-gray-200">
+                    Section 2
+                </section>
+            </div>
+        </div>
+
+
+
         <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+        <script type="text/javascript">
+            function dropdown() {
+                document.querySelector("#submenu").classList.toggle("hidden");
+                document.querySelector("#arrow").classList.toggle("rotate-0");
+            }
+            dropdown();
+
+            function openSidebar() {
+                document.querySelector(".sidebar").classList.toggle("hidden");
+            }
+        </script>
     </body>
 </html>
