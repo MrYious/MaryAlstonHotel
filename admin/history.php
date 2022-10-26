8e2c17f691b652f373c0491352e688eb240a2a61
MrYious <rosariomark37@gmail.com>
2022-10-26 21:06:43 +0800

    feat: updated favicon link

--- admin/history.php
+++ admin/history.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="js/jquery-3.6.1.min.js"></script>
-        <link rel="icon" href="images/favicon.png" type = "image/png">
+        <link rel="icon" href="/images/favicon.png" type = "image/png">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
