04acce659523588ccafde4d732a125d7d001b18a
MrYious <rosariomark37@gmail.com>
2022-10-30 21:04:17 +0800

    feat: updated mobile nav button z value

--- admin/reports/monthly.php
+++ admin/reports/monthly.php
@@ -32,7 +32,7 @@
     </head>
     <body>
         <!-- Mobile Nav Button -->
-        <span class="fixed text-white text-4xl top-5 left-4 cursor-pointer" onclick="openSidebar()" >
+        <span class="fixed text-white text-4xl top-5 left-4 cursor-pointer z-[2]" onclick="openSidebar()" >
             <div class="p-1 bg-gray-900 rounded-md">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
