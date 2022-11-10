76a79db5819c89aab5d2cbfa773a4b44017e727e
MrYious <rosariomark37@gmail.com>
2022-11-10 15:38:15 +0800

    feat: create updatePayment endpoint

---
+++ api/updatePayment.php
@@ -0,0 +1,17 @@
+<?php
+	include 'dbConnection.php';
+
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$id = $_POST['id'];
+		$paid = $_POST['paid'];
+
+        // QUERY
+		$sql = "UPDATE booking_tbl SET amountPaid='". $paid ."' WHERE id='". $id ."'";
+        $conn->query($sql);
+
+    }else{
+		header('location:/admin/settings.php');
+    }
+
+?>
