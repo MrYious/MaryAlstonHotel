610a7a66168322606f1944619aad3c9acfc393cc
MrYious <rosariomark37@gmail.com>
2022-11-14 17:10:36 +0800

    feat: updated quality

--- reservation.php
+++ reservation.php
@@ -1115,7 +1115,7 @@
                         margin:       1,
                         filename:     'Reservation.pdf',
                         image:        { type: 'jpeg', quality: 0.98 },
-                        html2canvas:  { scale: 1 },
+                        html2canvas:  { scale: 2 },
                         jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
                     };
                     html2pdf().set(opt).from(element).save();
