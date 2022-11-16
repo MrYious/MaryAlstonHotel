560190857af9239af60bd660fafe1eaf6f96005e
MrYious <rosariomark37@gmail.com>
2022-11-16 20:25:00 +0800

    feat: add monthly sales with dummy generate report

--- admin/reports/monthly.php
+++ admin/reports/monthly.php
@@ -6,7 +6,7 @@
 ?>
 <html>
     <head>
-        <title> Monthly Reports </title>
+        <title> Monthly Sales Reports </title>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://cdn.tailwindcss.com"></script>
@@ -30,6 +30,8 @@
                 }
             }
         </script>
+        <!-- HTML 2 PDF -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js" integrity="sha512-YcsIPGdhPK4P/uRW6/sruonlYj+Q7UHWeKfTAkBW+g83NKM+jMJFJ4iAPfSnVp7BKD4dKMHmVSvICUbE/V1sSw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     </head>
     <body>
         <!-- Mobile Nav Button -->
@@ -123,13 +125,87 @@
             <!-- CONTENT -->
             <div class="w-full">
                 <section class="flex py-7 px-20 lg:px-7 bg-gray-300">
-                    Admin / Monthly Report
+                    Admin / Monthly Sales Report
                 </section>
-                <section class="flex flex-col p-7 justify-center items-center bg-gray-200">
-                    Section 1
+                <section>
+                    <div class="flex justify-around items-center">
+                        <img src="/logo.jpg" alt="logo" width="100px" height="100px">
+                        <div class="text-center">
+                            <div class="font-bold">TRINITY UNIVERSITY OF ASIA</div>
+                            <div class="font-bold">MARY ALSTON HALL</div>
+                            <div>Room Reservation</div>
+                        </div>
+                        <img src="/logo.jpg" alt="logo" width="100px" height="100px">
+                    </div>
                 </section>
-                <section class="flex flex-col p-7 justify-center items-center bg-gray-200">
-                    Section 2
+                <section class="flex flex-col p-7 bg-gray-200 gap-10">
+                    <div class="text-lg">Generate and View the Monthly Sales Report</div>
+                    <div class="flex flex-col items-start gap-5">
+                        <div class="flex flex-col gap-2 w-60">
+                            <div class="font-bold text-lg">
+                                Select a year
+                            </div>
+                            <select id="year" required class="text-sm w-full lg:text-base browser-default px-2 py-2 border-[1px] focus:border-blue-800 outline-none rounded border-black">
+                                <option selected disabled>Select an option</option>
+                            </select>
+                        </div>
+                        <div class="flex flex-col gap-2 w-60">
+                            <div class="font-bold text-lg">
+                                Select a month
+                            </div>
+                            <select id="month" required class="text-sm w-full lg:text-base browser-default px-2 py-2 border-[1px] focus:border-blue-800 outline-none rounded border-black">
+                                <option selected disabled>Select an option</option>
+                                <option value="0">January</option>
+                                <option value="1">February</option>
+                                <option value="2">March</option>
+                                <option value="3">April</option>
+                                <option value="4">May</option>
+                                <option value="5">June</option>
+                                <option value="6">July</option>
+                                <option value="7">August</option>
+                                <option value="8">September</option>
+                                <option value="9">October</option>
+                                <option value="10">November</option>
+                                <option value="11">December</option>
+                            </select>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <div class="font-bold text-lg">
+                                Results
+                            </div>
+                            <div class="w-full">
+                                <div class="flex gap-1">
+                                    <div class="w-48">
+                                        Month:
+                                    </div>
+                                    <b id="date">
+                                       <!-- November 2022 -->
+                                    </b>
+                                </div>
+                                <div class="flex gap-1">
+                                    <div class="w-48">
+                                        Number of records:
+                                    </div>
+                                    <b id="records">
+                                       <!-- 20 -->
+                                    </b>
+                                </div>
+                                <div class="flex gap-1">
+                                    <div class="w-48">
+                                        Total Sales:
+                                    </div>
+                                    <b id="sales">
+                                        <!-- 10,900.00 -->
+                                    </b>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <button id="handleGenerate" class="font-bold text-lg bg-green-400 hover:bg-green-600 rounded py-2 px-5 shadow-sm shadow-black">
+                                GENERATE AND DOWNLOAD A COPY
+                            </button>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
@@ -150,6 +226,131 @@
             function openSidebar() {
                 document.querySelector(".sidebar").classList.toggle("hidden");
             }
+        </script>
+        <script>
+            var allReservations;
+            var selectedReservations;
+            var year, month;
+
+            // CONSTANTS
+            var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            var years = [];
+
+            // EVENT HANDLER
+            $('#year').on('change', function() {
+                year = this.value;
+                handleChange();
+            });
+            $('#month').on('change', function() {
+                month = this.value;
+                handleChange();
+            });
+
+            function handleChange() {
+                $("#date").text('');
+                $("#records").text('');
+                $("#sales").text('');
+                selectedReservations = [];
+
+                if(year && month){
+                    $("#date").text(months[month] + ' ' + year);
+
+                    selectedReservations = allReservations.filter( (reservation, i) =>{
+                        let thisDate = new Date(reservation.booking.inDate);
+                        console.log(thisDate.getFullYear(), year, thisDate.getMonth(), month);
+                        return thisDate.getFullYear() === parseInt(year) && thisDate.getMonth() === parseInt(month) && reservation.booking.bookingStatus === 'Completed';
+                    });
+                    console.log('SelectedReservations', selectedReservations);
+
+                    $("#records").text(selectedReservations.length);
+                    var totalSales = 0;
+                    selectedReservations.forEach(reservation => {
+                        let sale = parseInt(reservation.booking.amountPaid.replaceAll(',', ''));
+                        totalSales += sale;
+                    });
+                    console.log('Total Sales', totalSales);
+
+                    $("#sales").text('P ' + new Intl.NumberFormat().format(totalSales) + '.00');
+                }
+            }
+
+            $('#handleGenerate').click(function (e) {
+                e.preventDefault();
+                if(!year || !month){
+                    alert('Select a year and a month first')
+                }else if(!selectedReservations || selectedReservations.length === 0){
+                    alert('There are no records')
+                }else{
+                    alert('Processing...')
+                    var element = `
+                        <div class="flex justify-around items-center">
+                            <img src="/logo.jpg" alt="logo" width="100px" height="100px">
+                            <div class="text-center">
+                                <div>TRINITY UNIVERSITY OF ASIA</div>
+                                <div>MARY ALSTON HALL</div>
+                                <div>Room Reservation</div>
+                            </div>
+                            <img src="/logo.jpg" alt="logo" width="100px" height="100px">
+                        </div>
+                    `;
+                    var opt = {
+                        margin:       1,
+                        filename:     'Monthly Report - '+ months[month] + ' ' + year + '.pdf',
+                        image:        { type: 'jpeg', quality: 0.98 },
+                        html2canvas:  { scale: 2 },
+                        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+                    };
+                    html2pdf().set(opt).from(element).save();
+                }
+            })
+
+            // FUNCTIONS
+            function formatDate(date) {
+                return [
+                    date.getFullYear(),
+                    padTo2Digits(date.getMonth() + 1),
+                    padTo2Digits(date.getDate()),
+                ].join('-');
+            }
+
+            function padTo2Digits(num) {
+                return num.toString().padStart(2, '0');
+            }
+
+            // DATA FETCH
+            function loadData() {
+
+                $.post("/api/getAll.php")
+                .done(function(data, status) {
+                    // console.log('ALL RESERVATIONS', data)
+                    allReservations = data.bookings.map((booking) => {return {booking, guest: data.guests.find((guest) => { return booking.guest_id === guest.id })}});
+                    console.log('ALL RESERVATIONS', allReservations);
+
+                    var startYear = 2020;
+                    var currentYear = new Date().getFullYear();
+                    let years = []
+                    while ( startYear <= currentYear ) {
+                        years.push(startYear++);
+                    }
+                    years.sort(function(a, b) {
+                        return b - a;
+                    });
+
+                    console.log(startYear, currentYear, years);
+
+                    years.forEach(year => {
+                        $('#year').append($('<option>').val(year).text(year))
+                    });
+                }).fail(function() {
+                    alert( "Retrieval Error" );
+                    console.log('Retrieval Error')
+                })
+
+            }
+
+            // CALL IT FIRST TIME
+            loadData();
+
         </script>
     </body>
 </html>
