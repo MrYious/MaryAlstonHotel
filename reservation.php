47cbc75bcd14c635a0a84575a217fd174d1b543f
MrYious <rosariomark37@gmail.com>
2022-11-29 01:04:22 +0800

    feat: add checkbox and initial modal

--- reservation.php
+++ reservation.php
@@ -36,15 +36,62 @@
 
         <!-- HTML 2 PDF -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js" integrity="sha512-YcsIPGdhPK4P/uRW6/sruonlYj+Q7UHWeKfTAkBW+g83NKM+jMJFJ4iAPfSnVp7BKD4dKMHmVSvICUbE/V1sSw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <style>
+            [type="checkbox"].reset-checkbox,
+            [type="checkbox"].reset-checkbox:checked,
+            [type="checkbox"].reset-checkbox:not(checked) {
+            opacity: 1;
+            position: relative;
+            }
+
+            [type="checkbox"].reset-checkbox+span::before,
+            [type="checkbox"].reset-checkbox+span::after,
+            [type="checkbox"].reset-checkbox:checked+span::before,
+            [type="checkbox"].reset-checkbox:checked+span::after {
+            display: none;
+            }
+
+            [type="checkbox"].reset-checkbox+span:not(.lever) {
+            padding-left: 10px;
+            }
+        </style>
     </head>
     <body>
+        <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="exampleModalCenteredScrollable" tabindex="-1" aria-labelledby="exampleModalCenteredScrollable" aria-modal="true" role="dialog">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable relative w-auto pointer-events-none">
+                <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+                <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+                    <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalCenteredScrollableLabel">
+                    Modal title
+                    </h5>
+                    <button type="button"
+                    class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                    data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body relative p-4">
+                    <p>This is some placeholder content to show a vertically centered modal. We've added some extra copy here to show how vertically centering the modal works when combined with scrollable modals. We also use some repeated line breaks to quickly extend the height of the content, thereby triggering the scrolling. When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.</p>
+                <br><br><br><br><br><br><br><br><br><br>
+                <p>Just like that.</p>
+                </div>
+                <div
+                    class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+                    <button type="button"
+                    class="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+                    data-bs-dismiss="modal">
+                    Close
+                    </button>
+                    <button type="button"
+                    class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1">
+                    Save changes
+                    </button>
+                </div>
+                </div>
+            </div>
+        </div>
         <!-- TITLE BAR -->
         <div class="flex justify-between items-center bg-slate-600 h-[8vh] text-white">
             <a href="/" class="text-2xl font-semibold px-5">
                 Mary Alston Hotel - Reservation
-            </a>
-            <a href="/admin.php" class="flex items-center px-3   text-center h-full bg-green-600">
-                Go to Admin
             </a>
         </div>
         <!-- STEPPER -->
@@ -347,6 +394,16 @@
                                         </div>
                                     </div>
                                 </div>
+                                <label class="text-black flex items-center">
+                                    <input type="checkbox" class="filled-in reset-checkbox" />
+                                    <span>
+                                        I have read and agreed to the
+                                        <span id="privacy" class="font-bold hover:underline cursor-pointer">Privacy Policies</span>
+                                        and
+                                        <span id="terms" class="font-bold hover:underline cursor-pointer">Terms and Conditions</span>
+                                        ?
+                                    </span>
+                                </label>
                             </div>
                             <!-- 2ND COLUMN -->
                             <div class="flex lg:w-1/4 w-full ">
@@ -645,7 +702,7 @@
                 // STEPPER
                 var stepper = document.querySelector('.stepper');
                 var stepperInstance = new MStepper(stepper, {
-                    firstActive: 0,
+                    firstActive: 2,
                     validationFunction: validationFunction,
                     stepTitleNavigation: false,
                 })
@@ -888,6 +945,14 @@
                 $( "#finishBtn" ).click(function(e) {
                     e.preventDefault();
                     window.location.replace("/");
+                });
+
+                $( "#privacy" ).click(function() {
+                    alert('privacy');
+                });
+
+                $( "#terms" ).click(function() {
+                    alert('terms');
                 });
 
                 $("#generatePDF").click(function(e) {
@@ -1175,6 +1240,7 @@
 
                     updateTotal();
                 });
+
 
                 // DATA FETCH
                 function updateAllReservations(num) {
