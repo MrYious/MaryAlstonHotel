d56e85f42d9fb70dfc8040441be6b677784ca287
MrYious <rosariomark37@gmail.com>
2022-11-29 12:53:51 +0800

    feat: add checkbox required

--- reservation.php
+++ reservation.php
@@ -57,34 +57,45 @@
         </style>
     </head>
     <body>
-        <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="exampleModalCenteredScrollable" tabindex="-1" aria-labelledby="exampleModalCenteredScrollable" aria-modal="true" role="dialog">
-            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable relative w-auto pointer-events-none">
-                <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-                <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-                    <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalCenteredScrollableLabel">
-                    Modal title
-                    </h5>
-                    <button type="button"
-                    class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-                    data-bs-dismiss="modal" aria-label="Close"></button>
+        <div id="modalPrivacy" class=" flex justify-center items-center z-50 w-full h-screen fixed ">
+            <div class="w-full h-screen bg-gray-200 opacity-60"></div>
+            <div class="w-[90%] md:w-[50%] flex flex-col bg-gray-50 absolute drop-shadow-xl">
+                <div class="p-5 flex justify-between">
+                    <span class="font-bold text-2xl">Privacy Policy</span>
+                    <span class="closePrivacy cursor-pointer">❌</span>
                 </div>
-                <div class="modal-body relative p-4">
-                    <p>This is some placeholder content to show a vertically centered modal. We've added some extra copy here to show how vertically centering the modal works when combined with scrollable modals. We also use some repeated line breaks to quickly extend the height of the content, thereby triggering the scrolling. When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.</p>
-                <br><br><br><br><br><br><br><br><br><br>
-                <p>Just like that.</p>
+                <div class="flex flex-col gap-4 p-5 h-[400px] overflow-auto">
+                    <div class="font-semibold">Mary Alston Hotel's Privacy Policy</div>
+                    <div>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni tempore in sint distinctio blanditiis. Accusamus nostrum, sed magnam quae rerum nulla! Illo deserunt, dignissimos ut aut commodi blanditiis. Ex ab odit incidunt consectetur maiores culpa, adipisci molestias, ipsa laborum deserunt voluptates nihil nesciunt. Quam voluptates consequatur modi porro unde illum fugiat atque dicta, eius ut voluptatibus ipsam iure. Ad dicta perspiciatis excepturi reprehenderit eaque fugit sapiente ullam et! Commodi facere neque nesciunt reiciendis fugiat assumenda alias, esse aut obcaecati molestias impedit corporis quod quibusdam necessitatibus ipsa harum eaque laborum doloremque a autem voluptatibus ducimus unde, praesentium aliquid! Cupiditate, temporibus impedit.
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni tempore in sint distinctio blanditiis. Accusamus nostrum, sed magnam quae rerum nulla! Illo deserunt, dignissimos ut aut commodi blanditiis. Ex ab odit incidunt consectetur maiores culpa, adipisci molestias, ipsa laborum deserunt voluptates nihil nesciunt. Quam voluptates consequatur modi porro unde illum fugiat atque dicta, eius ut voluptatibus ipsam iure. Ad dicta perspiciatis excepturi reprehenderit eaque fugit sapiente ullam et! Commodi facere neque nesciunt reiciendis fugiat assumenda alias, esse aut obcaecati molestias impedit corporis quod quibusdam necessitatibus ipsa harum eaque laborum doloremque a autem voluptatibus ducimus unde, praesentium aliquid! Cupiditate, temporibus impedit.
+                    </div>
                 </div>
-                <div
-                    class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-                    <button type="button"
-                    class="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
-                    data-bs-dismiss="modal">
-                    Close
-                    </button>
-                    <button type="button"
-                    class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1">
-                    Save changes
-                    </button>
+                <div class="p-5 flex justify-end">
+                    <span class="closePrivacy cursor-pointer py-2 px-4 rounded font-semibold bg-red-500 text-red-100 hover:bg-red-700">CLOSE</span>
                 </div>
+            </div>
+        </div>
+        <div id="modalTerms" class=" flex justify-center items-center z-50 w-full h-screen fixed ">
+            <div class="w-full h-screen bg-gray-200 opacity-60"></div>
+            <div class="w-[90%] md:w-[50%] flex flex-col bg-gray-50 absolute drop-shadow-xl">
+                <div class="p-5 flex justify-between">
+                    <span class="font-bold text-2xl">Terms and Conditions</span>
+                    <span class="closeTerms cursor-pointer">❌</span>
+                </div>
+                <div class="flex flex-col gap-4 p-5 h-[400px] overflow-auto">
+                    <div class="font-semibold">Mary Alston Hotel's Terms and Conditions</div>
+                    <div>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni tempore in sint distinctio blanditiis. Accusamus nostrum, sed magnam quae rerum nulla! Illo deserunt, dignissimos ut aut commodi blanditiis. Ex ab odit incidunt consectetur maiores culpa, adipisci molestias, ipsa laborum deserunt voluptates nihil nesciunt. Quam voluptates consequatur modi porro unde illum fugiat atque dicta, eius ut voluptatibus ipsam iure. Ad dicta perspiciatis excepturi reprehenderit eaque fugit sapiente ullam et! Commodi facere neque nesciunt reiciendis fugiat assumenda alias, esse aut obcaecati molestias impedit corporis quod quibusdam necessitatibus ipsa harum eaque laborum doloremque a autem voluptatibus ducimus unde, praesentium aliquid! Cupiditate, temporibus impedit.
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni tempore in sint distinctio blanditiis. Accusamus nostrum, sed magnam quae rerum nulla! Illo deserunt, dignissimos ut aut commodi blanditiis. Ex ab odit incidunt consectetur maiores culpa, adipisci molestias, ipsa laborum deserunt voluptates nihil nesciunt. Quam voluptates consequatur modi porro unde illum fugiat atque dicta, eius ut voluptatibus ipsam iure. Ad dicta perspiciatis excepturi reprehenderit eaque fugit sapiente ullam et! Commodi facere neque nesciunt reiciendis fugiat assumenda alias, esse aut obcaecati molestias impedit corporis quod quibusdam necessitatibus ipsa harum eaque laborum doloremque a autem voluptatibus ducimus unde, praesentium aliquid! Cupiditate, temporibus impedit.
+                    </div>
+                </div>
+                <div class="p-5 flex justify-end">
+                    <span class="closeTerms cursor-pointer py-2 px-4 rounded font-semibold bg-red-500 text-red-100 hover:bg-red-700">CLOSE</span>
                 </div>
             </div>
         </div>
@@ -315,7 +326,7 @@
                     <div class="step-title waves-effect text-xl">Review & Confirm Reservation </div>
                     <div class="step-content flex flex-col">
                         <input type="text" class="id hidden" value="3">
-                        <div class="flex flex-col lg:flex-row h-fit gap-4 lg:gap-10 ">
+                        <div class="flex flex-col lg:flex-row h-fit gap-10 ">
                             <!-- 1ST COLUMN -->
                             <div class="flex flex-col h-fit gap-4 lg:w-3/4 w-full ">
                                 <!-- ROW 1 -->
@@ -395,7 +406,7 @@
                                     </div>
                                 </div>
                                 <label class="text-black flex items-center">
-                                    <input type="checkbox" class="filled-in reset-checkbox" />
+                                    <input id="privacyTerms" type="checkbox" class="filled-in reset-checkbox" />
                                     <span>
                                         I have read and agreed to the
                                         <span id="privacy" class="font-bold hover:underline cursor-pointer">Privacy Policies</span>
@@ -573,6 +584,10 @@
                 </li>
             </ul>
         </section>
+        <script>
+            $("#modalPrivacy").hide();
+            $("#modalTerms").hide();
+        </script>
         <script src="https://unpkg.com/materialize-stepper@3.1.0/dist/js/mstepper.min.js"></script>
         <script>
             document.addEventListener('DOMContentLoaded', function() {
@@ -924,22 +939,27 @@
 
                     let text = "Are you sure you want to submit this reservation?";
 
-                    if (confirm(text) == true) {
-                        $.post("/api/newBooking.php",{
-                            formData: formData,
-                            channel1,
-                            channel2,
-                            channel3
-                        }).done(function(data, status) {
-                            alert('Submission Success')
-                            stepperInstance.nextStep();
-                            formData.transCode = data.transactionCode;
-                            $("#transCode").text(data.transactionCode)
-                        }).fail(function() {
-                            alert('Submission Success')
-                            stepperInstance.nextStep();
-                        })
+                    if ($('#privacyTerms').is(":checked")){
+                        if (confirm(text) == true) {
+                            $.post("/api/newBooking.php",{
+                                formData: formData,
+                                channel1,
+                                channel2,
+                                channel3
+                            }).done(function(data, status) {
+                                alert('Submission Success')
+                                stepperInstance.nextStep();
+                                formData.transCode = data.transactionCode;
+                                $("#transCode").text(data.transactionCode)
+                            }).fail(function() {
+                                alert('Submission Success')
+                                stepperInstance.nextStep();
+                            })
+                        }
+                    } else{
+                        alert('To proceed, you must agree to our Privacy Policy and Terms and Conditions!')
                     }
+
                 });
 
                 $( "#finishBtn" ).click(function(e) {
@@ -948,11 +968,19 @@
                 });
 
                 $( "#privacy" ).click(function() {
-                    alert('privacy');
+                    $("#modalPrivacy").show();
+                });
+
+                $( ".closePrivacy" ).click(function() {
+                    $("#modalPrivacy").hide();
                 });
 
                 $( "#terms" ).click(function() {
-                    alert('terms');
+                    $("#modalTerms").show();
+                });
+
+                $( ".closeTerms" ).click(function() {
+                    $("#modalTerms").hide();
                 });
 
                 $("#generatePDF").click(function(e) {
